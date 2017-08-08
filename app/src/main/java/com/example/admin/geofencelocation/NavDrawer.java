7b19bde8e8d28d33073a3e595b1c4cdc559ec1e1
Yusuf <yegsoft@gmail.com>
2017-08-08 10:27:50 +0200

    olmaya devlet cihanda

--- app/src/main/java/com/example/admin/geofencelocation/NavDrawer.java
+++ app/src/main/java/com/example/admin/geofencelocation/NavDrawer.java
@@ -1,5 +1,6 @@
 package com.example.admin.geofencelocation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -81,7 +82,8 @@ public class NavDrawer extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
